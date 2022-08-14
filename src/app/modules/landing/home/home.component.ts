b804bb5baa3f74ccd3d8259dba2ef8839f9c74aa
anthonya <anthonya@cloudstaff.com>
2022-08-14 17:00:36 +0800

    Initial Commit

---
+++ src/app/modules/landing/home/home.component.ts
@@ -0,0 +1,17 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+    selector     : 'landing-home',
+    templateUrl  : './customer/public/index.component.html',
+    styleUrls: ['./customer/public/assets/css/theme.css'],
+    encapsulation: ViewEncapsulation.None
+})
+export class LandingHomeComponent
+{
+    /**
+     * Constructor
+     */
+    constructor()
+    {
+    }
+}
